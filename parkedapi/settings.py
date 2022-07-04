3f556227a57761bd5060d231e84b5206deff4311
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-04 23:06:23 +0100

    feat: added corsheaders, restframework and cleanup to installed apps

--- parkedapi/settings.py
+++ parkedapi/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+    ## Must stay at the bottom
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
