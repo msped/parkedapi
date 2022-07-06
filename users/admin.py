dc02fb73ba35570ad0845aa972728e7d7b08b0f2
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-06 22:12:37 +0100

    feat: added custom admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import Profile
 
 # Register your models here.
+
+class ProfileAdmin(UserAdmin):
+    model = Profile
+    list_display = [
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'date_joined',
+        'is_staff',
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': (
+            'profile_picture',
+            'private_account',
+            'website',
+            'bio',
+        )}),
+    )
+
+
+admin.site.register(Profile, ProfileAdmin)
