f144808364e44c39ef3360fc484fe339a1b3de17
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-06 22:12:19 +0100

    feat: added profile model to extend user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class Profile(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profile_pictures/")
+    private_account = models.BooleanField(default=False)
+    website = models.URLField()
+    bio = models.TextField(max_length=256)
