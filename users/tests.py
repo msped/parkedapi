6bca70070d07754072af81d21b896fc3007c751b
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-09 19:37:29 +0100

    test: added register test

--- users/tests.py
+++ users/tests.py
@@ -17,50 +17,50 @@ class AuthTests(APITestCase):
             password=make_password("5up3R!97")
         )
 
-    # def registration_working_response(self):
-    #     response = self.client.post(
-    #         '/api/auth/register/',
-    #         {
-    #             'username': 'test@gmail.com',
-    #             'password': '5up3R!98',
-    #             'password2': '5up3R!98'
-    #         }
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def registration_working_response(self):
+        response = self.client.post(
+            '/api/auth/register/',
+            {
+                'username': 'test@gmail.com',
+                'password': '5up3R!98',
+                'password2': '5up3R!98'
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    # def registration_invalid_email(self):
-    #     response = self.client.post(
-    #         '/api/auth/register/',
-    #         {
-    #             'username': 'test2gmail.com',
-    #             'password': '5up3R!98',
-    #             'password2': '5up3R!98'
-    #         }
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertEqual(
-    #         json.loads(response.content),
-    #         {
-    #             "username": ["Enter a valid email address."]
-    #         }
-    #     )
+    def registration_invalid_email(self):
+        response = self.client.post(
+            '/api/auth/register/',
+            {
+                'username': 'test2gmail.com',
+                'password': '5up3R!98',
+                'password2': '5up3R!98'
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            json.loads(response.content),
+            {
+                "username": ["Enter a valid email address."]
+            }
+        )
 
-    # def registration_passwords_dont_match(self):
-    #     response = self.client.post(
-    #         '/api/auth/register/',
-    #         {
-    #             'username': 'test3@gmail.com',
-    #             'password': '5up4R!98',
-    #             'password2': '5up4R!97'
-    #         }
-    #     )
-    #     self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-    #     self.assertEqual(
-    #         json.loads(response.content),
-    #         {
-    #             "password": ["Password fields don't match"]
-    #         }
-    #     )
+    def registration_passwords_dont_match(self):
+        response = self.client.post(
+            '/api/auth/register/',
+            {
+                'username': 'test3@gmail.com',
+                'password': '5up4R!98',
+                'password2': '5up4R!97'
+            }
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(
+            json.loads(response.content),
+            {
+                "password": ["Password fields don't match"]
+            }
+        )
 
     def request_access_token(self):
         response = self.client.post(
@@ -94,8 +94,8 @@ class AuthTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_in_order(self):
-        # self.registration_working_response()
-        # self.registration_invalid_email()
-        # self.registration_passwords_dont_match()
+        self.registration_working_response()
+        self.registration_invalid_email()
+        self.registration_passwords_dont_match()
         self.request_access_token()
         self.request_refresh_token()
