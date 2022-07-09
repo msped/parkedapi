4f3ead8d491e241f1a33272713f222ce394678ed
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-09 19:37:49 +0100

    feat: added register url path

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
-from .views import BlacklistTokenView
+from .views import RegisterView, BlacklistTokenView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/blacklist/', BlacklistTokenView.as_view(), name="register"),
+    path('token/blacklist/', BlacklistTokenView.as_view(), name="logout"),
+    path('register/', RegisterView.as_view(), name="register"),
 ]
