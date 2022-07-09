f897c7e4c6775eda1bde6b1a534b683f1a1f4804
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-09 19:20:50 +0100

    feat: added token blacklist url path

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
+from .views import BlacklistTokenView
+
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/blacklist/', BlacklistTokenView.as_view(), name="register"),
 ]
