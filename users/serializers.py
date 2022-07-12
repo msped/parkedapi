4de6552ad9a0fbad28b2c19c6a63193ece73dfc2
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-13 00:09:07 +0100

    fix: post names for registration to match

--- users/serializers.py
+++ users/serializers.py
@@ -5,16 +5,17 @@ from rest_framework.validators import UniqueValidator
 from .models import Profile
 
 class RegisterSerializer(serializers.ModelSerializer):
-    username = serializers.EmailField(
+    username = serializers.CharField(
         required=True,
         validators=[UniqueValidator(queryset=Profile.objects.all())]
     )
+    email= serializers.EmailField(required=True)
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = Profile
-        fields = ('id','username', 'password', 'password2')
+        fields = ('id','username', 'email', 'password', 'password2')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -24,7 +25,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = Profile.objects.create(
             username=validated_data['username'],
-            email=validated_data['username'],
+            email=validated_data['email'],
         )
 
         user.set_password(validated_data['password'])
