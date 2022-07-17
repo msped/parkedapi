601fc90ce92d1aa54c443b45ac657acc1d429b7a
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 22:26:54 +0100

    feat: removed registerview for djoser urls

--- users/views.py
+++ users/views.py
@@ -1,20 +1,14 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, UpdateAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.generics import UpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import Profile
 from .serializers import (
-    RegisterSerializer,
     ChangePasswordSerializer,
 )
-
-class RegisterView(CreateAPIView):
-    queryset = Profile.objects.all()
-    permission_classes = (AllowAny,)
-    serializer_class = RegisterSerializer
 
 class BlacklistTokenView(APIView):
     permission_classes = [IsAuthenticated]
