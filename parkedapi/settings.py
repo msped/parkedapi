142c279914f1fda4fd66248a229308c78a3f7bbe
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 20:12:10 +0100

    feat: added email setup to settings.py

--- parkedapi/settings.py
+++ parkedapi/settings.py
@@ -150,3 +150,10 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
