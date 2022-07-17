847eae15d34445042b3f0a37860014dce3528bc9
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 18:30:36 +0100

    fix: removed password reset urls

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
-from .views import RegisterView, BlacklistTokenView, ChangePasswordView
+from .views import (
+    RegisterView,
+    BlacklistTokenView,
+    ChangePasswordView,
+)
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
