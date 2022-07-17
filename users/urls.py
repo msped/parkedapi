2ced98fdd16fa3ac74a38ffcd18ca6312f7ac4b3
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 22:27:10 +0100

    feat: rem,oved register urls for djoser views

--- users/urls.py
+++ users/urls.py
@@ -1,20 +1,15 @@
-from django.urls import path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from django.urls import path, include
 
 from .views import (
-    RegisterView,
     BlacklistTokenView,
     ChangePasswordView,
 )
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/blacklist/', BlacklistTokenView.as_view(), name="logout"),
-    path('register/', RegisterView.as_view(), name="register"),
+    path('', include('djoser.urls.jwt')),
+    path('jwt/blacklist/', BlacklistTokenView.as_view(), name="logout"),
     path(
-        'change-password/<int:profile_id>/',
+        'change-password/',
         ChangePasswordView.as_view(),
         name="change_password"
     ),
