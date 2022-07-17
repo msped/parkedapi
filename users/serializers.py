6e511d52f7c57da130a50a5e17ee6c3e28c761a9
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 22:27:35 +0100

    feat: removed register serializer as now using djoser

--- users/serializers.py
+++ users/serializers.py
@@ -1,36 +1,4 @@
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-
-from .models import Profile
-
-class RegisterSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(
-        required=True,
-        validators=[UniqueValidator(queryset=Profile.objects.all())]
-    )
-    email= serializers.EmailField(required=True)
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)
-
-    class Meta:
-        model = Profile
-        fields = ('id','username', 'email', 'password', 'password2')
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Password fields don't match"})
-        return attrs
-
-    def create(self, validated_data):
-        user = Profile.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
 
 class ChangePasswordSerializer(serializers.Serializer):
     old_password = serializers.CharField(required=True)
