13309f650cf7bb330708740af76c4ea5f4ddd9df
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 18:29:39 +0100

    feat: added changePassword serializer

--- users/serializers.py
+++ users/serializers.py
@@ -31,3 +31,15 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    new_password2 = serializers.CharField(required=True)
+
+    def validate(self, attrs):
+        if attrs['new_password'] != attrs['new_password2']:
+            raise serializers.ValidationError("Passwords don't match.")
+        if attrs['old_password'] == attrs['new_password'] or attrs['old_password'] == attrs['new_password2']:
+            raise serializers.ValidationError("New password(s) must be different.")
+        return attrs
