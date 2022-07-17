56d2ac7a05c8f6b655adcac65dc1390e786903d6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-17 20:20:53 +0100

    feat: added simple_jwt config

--- parkedapi/settings.py
+++ parkedapi/settings.py
@@ -151,6 +151,10 @@ REST_FRAMEWORK = {
     ),
 }
 
+SIMPLE_JWT = {
+   'AUTH_HEADER_TYPES': ('JWT',),
+}
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
