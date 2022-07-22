a760de620ebb1d3566f369f130219e6cac138ab6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-07-23 00:30:06 +0100

    refactor: cahnged view to user current user

--- users/views.py
+++ users/views.py
@@ -27,17 +27,17 @@ class ChangePasswordView(UpdateAPIView):
     model = Profile
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, profile_id):
-        obj = Profile.objects.get(id=profile_id)
+    def post(self, request):
         serializer = self.get_serializer(data=request.data)
-
+        current_user = request.user
+        profile = Profile.objects.get(id=current_user.id)
         if serializer.is_valid():
-            if not obj.check_password(serializer.data.get("old_password")):
+            if not profile.check_password(serializer.data.get("old_password")):
                 return Response(
                     {"error": ["Wrong password."]},
                     status=status.HTTP_400_BAD_REQUEST
                 )
-            obj.set_password(serializer.data.get("new_password"))
-            obj.save()
+            profile.set_password(serializer.data.get("new_password"))
+            profile.save()
             return Response(status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
