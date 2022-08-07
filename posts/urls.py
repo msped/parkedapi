916d689e94828f9080022abdc616d0724cae5c26
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-07 20:00:20 +0100

    feat: added CommentNew url

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PostNew
+from .views import PostNew, CommentNew
 
 urlpatterns = [
     path('post/new/', PostNew.as_view(), name="post"),
+    path('comment/new/', CommentNew.as_view(), name="comment"),
 ]
