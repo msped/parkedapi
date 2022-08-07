6161ff7e830afd5d69dc4ce1d21c9cd711f659e6
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-07 15:54:45 +0100

    feat: added post urls to project urls

--- parkedapi/urls.py
+++ parkedapi/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('users.urls'), name="auth")
+    path('api/auth/', include('users.urls'), name="auth"),
+    path('api/posts/', include("posts.urls"), name="posts"),
 ]
