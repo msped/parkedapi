2735986507d603a2cc5d2a18c09285807983345c
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-07 19:58:46 +0100

    feat: add PostNew view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .models import Comment, CommentLikes, Post, PostLikes
+from .serializers import CommentSerializer, PostSerializer
 
 # Create your views here.
+
+class PostNew(CreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = Post.objects.all()
+
