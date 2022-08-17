26d4cb29a5be5fe2f922938f6799075be8751cfb
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-17 23:23:36 +0100

    feat: added comment and post like urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostNew, CommentNew
+from .views import PostNew, CommentNew, PostLike, CommentLike
 
 urlpatterns = [
     path('new/', PostNew.as_view(), name="post"),
     path('comment/new/', CommentNew.as_view(), name="comment"),
-    # path('post/like/<slug:slug>/', PostLike.as_view(), name="post_like"),
-    # path('comment/like/<slug:slug>/', CommentLike.as_view(), name="comment_like"),
+    path('post/like/<slug:slug>/', PostLike.as_view(), name="like_post"),
+    path('comment/like/<slug:slug>/', CommentLike.as_view(), name="like_comment"),
 ]
