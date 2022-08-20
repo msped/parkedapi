ca70e8795cddde4815f9d7d1b8550ab572b36bb7
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-20 22:08:46 +0100

    feat: added comment like view

--- posts/views.py
+++ posts/views.py
@@ -5,7 +5,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Comment, CommentLikes, Post, PostLikes
-from .serializers import CommentSerializer, PostSerializer, PostLikeSerializer
+from .serializers import (
+    CommentSerializer,
+    PostSerializer,
+    PostLikeSerializer,
+    CommentsLikeSerializer
+)
 
 # Create your views here.
 
@@ -27,6 +32,21 @@ class PostLike(APIView):
         # Create a post like obj or delete it
         obj, created = PostLikes.objects.get_or_create(
             post__slug=slug,
+            profile__id=request.user.id
+        )
+        if created:
+            return Response(status=status.HTTP_201_CREATED)
+        obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommentLike(APIView):
+    serializer_class = CommentsLikeSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, slug):
+        # Create a post like obj or delete it
+        obj, created = CommentLikes.objects.get_or_create(
+            comment__slug=slug,
             profile__id=request.user.id
         )
         if created:
