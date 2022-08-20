f163f51a1ebbcf80dd0bd870fe3f5ac93383ea3b
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-20 22:08:34 +0100

    refactor: removed post from like url

--- posts/urls.py
+++ posts/urls.py
@@ -4,6 +4,6 @@ from .views import PostNew, CommentNew, PostLike, CommentLike
 urlpatterns = [
     path('new/', PostNew.as_view(), name="post"),
     path('comment/new/', CommentNew.as_view(), name="comment"),
-    path('post/like/<slug:slug>/', PostLike.as_view(), name="like_post"),
+    path('like/<slug:slug>/', PostLike.as_view(), name="like_post"),
     path('comment/like/<slug:slug>/', CommentLike.as_view(), name="like_comment"),
 ]
