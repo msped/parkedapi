3477775b4e2fea83588df3aea479bd2b64208713
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-21 23:19:05 +0100

    feat: added slug to new comment url

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from .views import PostNew, CommentNew, PostLike, CommentLike
 
 urlpatterns = [
     path('new/', PostNew.as_view(), name="post"),
-    path('comment/new/', CommentNew.as_view(), name="comment"),
+    path('<slug:slug>/comment/new/', CommentNew.as_view(), name="comment"),
     path('like/<slug:slug>/', PostLike.as_view(), name="like_post"),
     path('comment/like/<slug:slug>/', CommentLike.as_view(), name="like_comment"),
 ]
