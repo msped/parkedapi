b9b0a8f399bd60dfd3b92fe3db4041cc1399a20e
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-22 23:23:08 +0100

    feat: added comment url with comment_id param

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostNew, CommentNew, PostLike, CommentLike
+from .views import PostNew, CommentNew, PostLike, CommentLike, CommentGetUpdateDestroy
 
 urlpatterns = [
     path('new/', PostNew.as_view(), name="post"),
     path('<slug:slug>/comment/new/', CommentNew.as_view(), name="comment"),
     path('like/<slug:slug>/', PostLike.as_view(), name="like_post"),
     path('comment/like/<int:comment_id>/', CommentLike.as_view(), name="like_comment"),
+    path('comment/<int:comment_id>/', CommentGetUpdateDestroy.as_view(), name="comment"),
 ]
