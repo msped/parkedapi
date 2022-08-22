b4baa5d65dfd830fb80c8169985f69fb74c4ef7e
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-22 20:55:44 +0100

    refactor: change comment url from slug to id

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('new/', PostNew.as_view(), name="post"),
     path('<slug:slug>/comment/new/', CommentNew.as_view(), name="comment"),
     path('like/<slug:slug>/', PostLike.as_view(), name="like_post"),
-    path('comment/like/<slug:slug>/', CommentLike.as_view(), name="like_comment"),
+    path('comment/like/<int:comment_id>/', CommentLike.as_view(), name="like_comment"),
 ]
