e1157207427195251e951d2b750240d8c5c1088a
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-07 22:04:30 +0100

    feat: added following manytomany to profile model

--- users/models.py
+++ users/models.py
@@ -8,3 +8,12 @@ class Profile(AbstractUser):
     private_account = models.BooleanField(default=False)
     website = models.URLField(null=True, blank=True)
     bio = models.TextField(max_length=256, null=True, blank=True)
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.username}'s Profile"
