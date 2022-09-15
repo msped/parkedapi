38bd48d8fc2e78ec2228ccf3d9e9457ccc2ec4db
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-15 23:41:10 +0100

    feat: added Post, Comment and Like models to admin site

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Post, PostLikes, Comment, CommentLikes
+
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostLikes)
+admin.site.register(CommentLikes)
