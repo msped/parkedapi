77b9479efef6d07a797a9070d730c81b4077312b
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-18 21:34:41 +0100

    feat: added mark as read view

--- notifications/views.py
+++ notifications/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Notification
+
+class MarkAsRead(APIView):
+    def get(self, request, notificaition_id):
+        notification = get_object_or_404(
+            Notification,
+            id=notificaition_id
+        )
+        notification.mark_as_read()
+        return Response(status=status.HTTP_200_OK)
