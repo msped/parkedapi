918e13d5c1c0a9965a0dd4ba803275f5448d43c2
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-18 21:40:23 +0100

    feat: added mark all as read view

--- notifications/views.py
+++ notifications/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from users.models import Profile
 
 from .models import Notification
+
 
 class MarkAsRead(APIView):
     def get(self, request, notification_id):
@@ -21,4 +23,10 @@ class MarkAsUnread(APIView):
             id=notification_id
         )
         notification.mark_as_unread()
+        return Response(status=status.HTTP_200_OK)
+
+class MarkAllAsRead(APIView):
+    def get(self, request):
+        profile = get_object_or_404(Profile, id=request.user.id)
+        Notification.objects.mark_all_as_read(recipient=profile)
         return Response(status=status.HTTP_200_OK)
