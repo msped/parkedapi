6dfca93383f7fbf2fe420c2cd023d1125b7f00fe
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-18 21:36:36 +0100

    feat: added mark as unread url

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import MarkAsRead
+from .views import MarkAsRead, MarkAsUnread
 
 urlpatterns = [
     path('read/<int:notification_id>/', MarkAsRead.as_view(), name="mark_as_read"),
+    path(
+        'unread/<int:notification_id>/',
+        MarkAsUnread.as_view(),
+        name="mark_as_unread"
+    ),
 ]
