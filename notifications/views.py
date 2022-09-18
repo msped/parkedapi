afaa494332e0d6988f5451a712fc124d337a4f89
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-18 21:36:44 +0100

    feat: added mark as unread view

--- notifications/views.py
+++ notifications/views.py
@@ -6,10 +6,19 @@ from rest_framework.views import APIView
 from .models import Notification
 
 class MarkAsRead(APIView):
-    def get(self, request, notificaition_id):
+    def get(self, request, notification_id):
         notification = get_object_or_404(
             Notification,
-            id=notificaition_id
+            id=notification_id
         )
         notification.mark_as_read()
+        return Response(status=status.HTTP_200_OK)
+
+class MarkAsUnread(APIView):
+    def get(self, request, notification_id):
+        notification = get_object_or_404(
+            Notification,
+            id=notification_id
+        )
+        notification.mark_as_unread()
         return Response(status=status.HTTP_200_OK)
