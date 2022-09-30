fde1b94637018d0acd56cd0e9c40c6aba93698ab
Matt Edwards <mattsp.edwards@googlemail.com>
2022-09-30 23:12:05 +0100

    feat: added http to routing.py

--- parkedapi/routing.py
+++ parkedapi/routing.py
@@ -1,11 +1,12 @@
-from django.urls import path
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-
+from django.core.asgi import get_asgi_application
+from django.urls import path
 from notifications.consumers import NotificationConsumer
 
 application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
