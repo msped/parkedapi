75e8c1b19e90411ed0c56620b9c3a183efc445ac
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-09 22:56:29 +0100

    fix: connected notification handler in apps.py

--- notifications/apps.py
+++ notifications/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
-
+from .signals import notify
 
 class NotificationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notifications'
 
     def ready(self):
-        import notifications.signals
+        from .handler import notify_handler
+        notify.connect(notify_handler)
