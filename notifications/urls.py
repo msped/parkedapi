396e1c4bf2e823422b00dfb2f99c822d2fb7e9ac
Matt Edwards <mattsp.edwards@googlemail.com>
2022-10-20 21:40:06 +0100

    feat: added get notification url

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import MarkAsRead, MarkAsUnread, MarkAllAsRead, GetAll
+from .views import (GetAll, GetNotification, MarkAllAsRead, MarkAsRead,
+                    MarkAsUnread)
 
 urlpatterns = [
     path('read/<int:notification_id>/', MarkAsRead.as_view(), name="mark_as_read"),
@@ -11,4 +12,5 @@ urlpatterns = [
     ),
     path('read/all/', MarkAllAsRead.as_view(), name="mark_all_as_read"),
     path('all/', GetAll.as_view(), name="get_all_notifications"),
+    path('<int:pk>/', GetNotification.as_view(), name="get_notification"),
 ]
